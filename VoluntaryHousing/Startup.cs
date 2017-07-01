58c6a668ab968378237f3c54946f9c0af12faf8a
wangg02 <wangg02@WH-PC225>
2017-07-01 11:12:12 +0800

    1111

---
+++ VoluntaryHousing/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(VoluntaryHousing.Startup))]
+namespace VoluntaryHousing
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
